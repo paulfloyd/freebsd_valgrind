8edf6527fb096777b8d7a94590b5c5da6fbbfdce
Paul Floyd <pjfloyd@wanadoo.fr>
2020-05-03 21:24:56 +0200

    Typo

--- include/pub_tool_aspacemgr.h
+++ include/pub_tool_aspacemgr.h
@@ -110,7 +110,7 @@ typedef
       Bool    hasT;     // True --> translations have (or MAY have)
                         // been taken from this segment
       Bool    isCH;     // True --> is client heap (SkAnonC ONLY)
-// @todo PJF Matin Johnston's patch
+// @todo PJF Martin Johnston's patch
      // Bool    isFF;     // True --> is a fixed file mapping
    }
    NSegment;
