b03d260cb9fe0dbb3761541ff77ecd2e9e12df69
Paul Floyd <pjfloyd@wanadoo.fr>
2020-05-14 21:03:09 +0200

    Yet another change. Is there some random file descriptor that gets tacked on?

--- none/tests/rlimit_nofile.c
+++ none/tests/rlimit_nofile.c
@@ -91,7 +91,7 @@ int main(int argc, char **argv)
               (unsigned long long)oldrlim.rlim_max);
    }
 
-#if defined(VGP_amd64_freebsd)
+#if defined(VGP_amd64_freebsd) && !defined(__clang__)
    // some other file open?
    newrlim.rlim_cur -= 4;
 #else
