f88e9419afff199bb1d0a2dc665e61231c1278d6
Paul Floyd <pjfloyd@wanadoo.fr>
2020-05-20 21:49:38 +0200

    Sneak in another syscall

--- memcheck/tests/freebsd/revoke.c
+++ memcheck/tests/freebsd/revoke.c
@@ -7,8 +7,14 @@
  {
      const char* filename = strdup("revoke.tst");
      int fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-     
+
+     // add a gratuitous syscall
+     (void)getdtablesize();
+    
+     // OK 
      revoke(filename);
+
+     // with an invalid param
      free(filename);
      revoke(filename);
  }
