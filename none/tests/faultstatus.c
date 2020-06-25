2d7837517638a12c3bba911c5fdfcdf15d08ebe2
Paul Floyd <pjfloyd@wanadoo.fr>
2020-06-25 16:37:16 +0200

    These tests depend on faultstatus which has an issue with FreeBSD's non-Posix hadnling of access-beyond-mmap errors (Posix says they should generate SIGBUS but FreeBSD generates a SIGSEGV).

--- none/tests/faultstatus.c
+++ none/tests/faultstatus.c
@@ -166,7 +166,11 @@ int main()
 #define T(n, sig, code, addr) { test##n, sig, code, addr }
 			T(1, SIGSEGV,	SEGV_MAPERR,	BADADDR),
 			T(2, SIGSEGV,	SEGV_ACCERR,	mapping),
+#if defined(VGO_freebsd)
+			T(3, SIGSEGV,	BUS_ERROR_SI_CODE, &mapping[FILESIZE+10]),
+#else
 			T(3, SIGBUS,	BUS_ERROR_SI_CODE, &mapping[FILESIZE+10]),
+#endif			
 			T(4, SIGFPE,    DIVISION_BY_ZERO_SI_CODE, 0),
 #undef T
 		};
