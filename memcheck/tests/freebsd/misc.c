248e74874556a8c0f6abd198c5ef3a9e4684191b
Paul Floyd <pjfloyd@wanadoo.fr>
2020-07-06 11:40:32 +0200

    Fix misc testcase compilation on FreeBSD 11

--- memcheck/tests/freebsd/misc.c
+++ memcheck/tests/freebsd/misc.c
@@ -11,6 +11,7 @@
 #include <sys/random.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../../config.h"
 
 int main(void)
 {
@@ -25,6 +26,8 @@ int main(void)
    {
       perror("uuidgen failed:");
    }
+   
+#if (FREEBSD_VERS >= FREEBSD_12)
 
    char buf[100];
    if (-1 == getrandom(buf, 100, GRND_NONBLOCK))
@@ -32,17 +35,23 @@ int main(void)
       perror("getrandom failed:");
    }
    
+#endif   
+   
    // error section
    struct uuid* ps = malloc(2*sizeof(struct uuid));
    uuidgen(ps, 3);
    int badint;
    uuidgen(&s, badint);
    
+#if (FREEBSD_VERS >= FREEBSD_12)
+
    char* buf2 = malloc(100);
    free(buf2);
    
    getrandom(buf2, 100, 0);
    getrandom(buf, badint, badint);
+   
+#endif   
    
    free(ps);
    
