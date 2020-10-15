508da5c74a57a4c9c94a6b43ae7dcf0fe8a529c6
Paul Floyd <pjfloyd@wanadoo.fr>
2020-10-15 21:56:12 +0200

    Static test for calloc,realloc and reallocf

---
+++ memcheck/tests/freebsd/static_allocs.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+static char buf[10000];
+static int bufi = 0;
+void *realloc(void *ptr, size_t size) {
+   if (!ptr) {
+      bufi += size;
+      return buf + bufi - size;
+   } else {
+      return NULL;
+   }
+}
+
+void *calloc(size_t number, size_t size) {
+   bufi += number*size;
+   
+   return buf + bufi - (number*size);
+}
+
+
+void free(void*ptr) {
+}
+
+void *reallocf(void *ptr, size_t size) {
+   return ptr;
+}
+
+
+int main (void)
+{
+   char *p;
+   p = realloc(NULL, 10);
+   p = calloc(10, 16);
+   (void)reallocf(p, 0);
+   return 0;
+}
+
